a722d08835a15723217d00b8f8e457b9e18da369
rahul-gitt <postbox9635@gmail.com>
2026-06-23 01:21:26 +0530

    count frequency using dictionary

--- dict03-count_frequency.py
+++ dict03-count_frequency.py
@@ -1,4 +1,4 @@
-# count the frequency of each elements
+# count the frequency of each elements in a list using dictionaries
 a = [1,1,1,1,2,2,2,2,3,4,4,4,4,5,5,5,5,5,0,0,0,0,6,6,6,6,6,7,8,9,9]
 d1= {}
 for i in a:
